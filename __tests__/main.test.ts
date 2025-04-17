1ea2b76252ca90468337b51c04762d0746bb307c
harehare <harehare1110@gmail.com>
2025-04-17 22:01:51 +0900

    :bug: Fix tests

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,126 +1,128 @@
-import { describe, it, expect, vi, beforeEach, afterEach } from 'vitest'
+import { describe, it, expect, vi, beforeEach, afterEach } from "vitest";
 
-vi.stubGlobal('process', {
-  arch: 'x64',
-  platform: 'linux'
-})
+vi.stubGlobal("process", {
+  arch: "x64",
+  platform: "linux",
+  env: {},
+});
 
-vi.mock('process', () => {
-  return {
-    arch: vi.fn(() => 'x64'),
-    platform: vi.fn(() => 'linux')
-  }
-})
+vi.mock("fs", () => ({
+  ...vi.importActual("fs"),
+  promises: {
+    copyFile: vi.fn(),
+    chmod: vi.fn(),
+  },
+}));
 
-vi.mock('@actions/core', () => {
+vi.mock("@actions/core", () => {
   const getInput = vi.fn((name) => {
-    if (name === 'version') return 'v0.1.0'
-    if (name === 'github-token') return 'fake-token'
-    return ''
-  })
+    if (name === "version") return "v0.1.0";
+    if (name === "github-token") return "fake-token";
+    return "";
+  });
 
   return {
     getInput,
     info: vi.fn(),
     error: vi.fn(),
     setFailed: vi.fn(),
-    addPath: vi.fn()
-  }
-})
+    addPath: vi.fn(),
+  };
+});
 
-vi.mock('@actions/tool-cache', () => {
+vi.mock("@actions/tool-cache", () => {
   return {
     downloadTool: vi.fn((path) => {
-      if (path === 'latest_url') return 'latest_tool'
-      if (path === 'v0.1.0_url') return 'v0.1.0_tool'
-      return ''
+      if (path === "latest_url/mq") return "latest_tool/mq";
+      if (path === "v0.1.0_url/mq") return "v0.1.0_tool/mq";
+      return "";
     }),
-    extractTar: vi.fn().mockResolvedValue('/path/to/extracted/directory'),
-    find: vi.fn().mockReturnValue(''),
+    extractTar: vi.fn().mockResolvedValue("/path/to/extracted/directory"),
+    find: vi.fn().mockReturnValue(""),
     cacheDir: vi.fn((path) => {
-      return path
+      return path;
     }),
-    cacheFile: vi.fn().mockResolvedValue('/path/to/cached/file'),
-    addPath: vi.fn()
-  }
-})
+    cacheFile: vi.fn().mockResolvedValue("/path/to/cached/file"),
+    addPath: vi.fn(),
+  };
+});
 
-vi.mock('@actions/github', () => ({
+vi.mock("@actions/github", () => ({
   getOctokit: vi.fn(() => ({
     rest: {
       repos: {
         getLatestRelease: vi.fn().mockResolvedValue({
           data: {
-            tag_name: 'v0.1.0',
+            tag_name: "v0.1.0",
             assets: [
               {
-                browser_download_url: 'latest_url',
-                name: 'mq-x86_64-unknown-linux-gnu'
-              }
-            ]
-          }
+                browser_download_url: "latest_url/mq",
+                name: "mq-x86_64-unknown-linux-gnu",
+              },
+            ],
+          },
         }),
         getReleaseByTag: vi.fn().mockResolvedValue({
           data: {
-            tag_name: 'v0.1.0',
+            tag_name: "v0.1.0",
             assets: [
               {
-                browser_download_url: 'v0.1.0_url',
-                name: 'mq-x86_64-unknown-linux-gnu',
-                version: 'v0.1.0'
-              }
-            ]
-          }
-        })
-      }
-    }
-  }))
-}))
+                browser_download_url: "v0.1.0_url/mq",
+                name: "mq-x86_64-unknown-linux-gnu",
+                version: "v0.1.0",
+              },
+            ],
+          },
+        }),
+      },
+    },
+  })),
+}));
 
-import { run } from '../src/main.js'
-import * as core from '@actions/core'
-import * as github from '@actions/github'
+import { run } from "../src/main.js";
+import * as core from "@actions/core";
+import * as github from "@actions/github";
 
-describe('GitHub Action', () => {
+describe("GitHub Action", () => {
   beforeEach(() => {
-    vi.clearAllMocks()
-  })
+    vi.clearAllMocks();
+  });
 
   afterEach(() => {
-    vi.resetAllMocks()
-  })
+    vi.resetAllMocks();
+  });
 
-  it('should execute correctly with a specified version', async () => {
+  it("should execute correctly with a specified version", async () => {
     vi.mocked(core.getInput).mockImplementation((name) => {
-      if (name === 'version') return 'v0.1.0'
-      if (name === 'github-token') return 'fake-token'
-      return ''
-    })
+      if (name === "version") return "v0.1.0";
+      if (name === "github-token") return "fake-token";
+      return "";
+    });
 
-    await run()
+    await run();
 
-    expect(core.getInput).toHaveBeenCalledWith('version')
-    expect(core.getInput).toHaveBeenCalledWith('github-token')
-    expect(github.getOctokit).toHaveBeenCalledWith('fake-token')
+    expect(core.getInput).toHaveBeenCalledWith("version");
+    expect(core.getInput).toHaveBeenCalledWith("github-token");
+    expect(github.getOctokit).toHaveBeenCalledWith("fake-token");
 
     // Verify that getReleaseByTag was called with the specified version
-    expect(core.addPath).toHaveBeenCalledWith('v0.1.0_tool')
-  })
+    expect(core.addPath).toHaveBeenCalledWith("v0.1.0_tool");
+  });
 
-  it('should execute correctly with a no specified version', async () => {
+  it("should execute correctly with a no specified version", async () => {
     vi.mocked(core.getInput).mockImplementation((name) => {
-      if (name === 'version') return ''
-      if (name === 'github-token') return 'fake-token'
-      return ''
-    })
+      if (name === "version") return "";
+      if (name === "github-token") return "fake-token";
+      return "";
+    });
 
-    await run()
+    await run();
 
-    expect(core.getInput).toHaveBeenCalledWith('version')
-    expect(core.getInput).toHaveBeenCalledWith('github-token')
-    expect(github.getOctokit).toHaveBeenCalledWith('fake-token')
+    expect(core.getInput).toHaveBeenCalledWith("version");
+    expect(core.getInput).toHaveBeenCalledWith("github-token");
+    expect(github.getOctokit).toHaveBeenCalledWith("fake-token");
 
     // Verify that getLatestRelease was called with the specified version
-    expect(core.addPath).toHaveBeenCalledWith('latest_tool')
-  })
-})
+    expect(core.addPath).toHaveBeenCalledWith("latest_tool");
+  });
+});
