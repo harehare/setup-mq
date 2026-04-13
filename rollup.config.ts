2715cd2b75d3b3c4d894a8e0f83d707246c5002a
harehare <harehare1110@gmail.com>
2026-04-13 21:13:05 +0900

    ✨ chore: bump version to 1.1.4 and update rollup config

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,8 +1,9 @@
 import commonjs from '@rollup/plugin-commonjs';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
+import { defineConfig } from 'rollup';
 
-const config = {
+const config = defineConfig({
   input: 'src/index.ts',
   output: {
     esModule: true,
@@ -12,6 +13,11 @@ const config = {
     compact: true,
   },
   plugins: [typescript(), nodeResolve({ preferBuiltins: true }), commonjs()],
-};
+  onwarn(warning, warn) {
+    if (warning.code === 'THIS_IS_UNDEFINED') return;
+    if (warning.code === 'CIRCULAR_DEPENDENCY' && warning.ids?.every(id => id.includes('node_modules'))) return;
+    warn(warning);
+  },
+});
 
 export default config;
